9de77e207c4efcece3f9791c6b3294d5a73f2e37
Andrew Bourgeois <andrew@olympicmedia.com>
2020-04-18 19:22:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,38 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+# –––––––––––––––––––––––––––––
+def roll_call_dwarves(dwarves)
+  i = 0
+  while i < dwarves.length
+    dwarves.each do |dwarf|
+      dwarf_index = (dwarves.index(dwarf) + 1).to_s
+      puts "#{dwarf_index}. #{dwarf}"
+    end
+    i += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+# –––––––––––––––––––––––––––––
+def summon_captain_planet(elements)
+  elements.map do |element|
+    element.capitalize() + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+# –––––––––––––––––––––––––––––
+def long_planeteer_calls(calls)
+  # calls_to_string = calls.join(" ")
+  # check = /\w{4,}/.match(calls_to_string)
+  # if check == nil
+  #   false
+  # else
+  #   true
+  # end
+  calls.any? do |call|
+    call.length >= 4
+  end
 end
 
 def find_the_cheese# code an argument here
